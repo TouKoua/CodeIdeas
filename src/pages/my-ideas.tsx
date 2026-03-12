84d26ebe80d3c2eaacdc92460ae983a22fbbee95
TouKoua <tou-koua1@hotmail.com>
2026-03-11 23:31:54 -0500

    WIP Delete Ideas
    Working on deleting ideas from the database. currently trying to figure out how to send a string from comparison with UUIDs in supabase functions

--- src/pages/my-ideas.tsx
+++ src/pages/my-ideas.tsx
@@ -5,6 +5,7 @@ import "./myIdeas.css";
 import type { Idea } from "../types/index.ts";
 import ProjectCard from "../components/ProjectCard";
 import { Link } from "react-router-dom";
+import supabase from "../services/supabaseClient.ts";
 
 function MyIdeas() {
   const { user, userProfile } = useAuth();
@@ -19,6 +20,28 @@ function MyIdeas() {
     fetchIdeas();
   }, [user]);
 
+  const handleDeleteIdea = async (ideaId: string) => {
+    // Implement idea deletion logic here
+    // After deletion, refetch the ideas to update the UI
+    if (!window.confirm("Are you sure you want to delete this idea?")) {
+      return;
+    }
+    try {
+      const { error } = await supabase.rpc("deleteidea", {
+        ideaid: ideaId,
+      });
+      if (error) {
+        throw error;
+      }
+    } catch (err: any) {
+      alert("Error deleting idea: " + err.message);
+      return;
+    }
+
+    const userIdeas = await fetchUserIdeas(user);
+    setIdeas(userIdeas);
+  };
+
   return (
     <div className="my-ideas-page">
       <h1>My Ideas</h1>
@@ -30,6 +53,7 @@ function MyIdeas() {
               <Link to={`/project/${idea.id}`} className="project-link">
                 <ProjectCard project={idea} />
               </Link>
+              <button onClick={() => handleDeleteIdea(idea.id)}>Delete</button>
             </div>
           ))}
         </div>
