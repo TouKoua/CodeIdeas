407c47db1b7586d72f677f06d64a75f75be052e5
Nathan Huang <nh060501@gmail.com>
2026-03-24 16:30:09 -0500

    Minor Dropdown Changes
    Profile and logout button is under the button now.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,15 +2,39 @@ import { Link } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
 import { useNavigate } from "react-router-dom";
 import "./Header.css";
+import { useState } from "react";
 
 function Header() {
   const { user, signOut } = useAuth();
   const navigate = useNavigate();
   const homeLink = user ? "/dashboard" : "/";
+  const [isDropdownVisible, setDropdownVisible] = useState(false);
 
-  const handleLogOut = async () => {
-    await signOut();
-    navigate("/");
+  const handleMouseEnter = () => {
+    setDropdownVisible(true);
+  };
+
+  const handleMouseLeave = () => {
+    setDropdownVisible(false);
+  };
+
+  const profileDropdown = () => {
+    return (
+      <div className="dropdown-menu">
+        <ul className="dropdown-list">
+          <li>
+            <Link to="/profile" className="dropdown-link">
+              My Profile
+            </Link>
+          </li>
+          <li>
+            <Link to="/" className="dropdown-link" onClick={signOut}>
+              Log Out
+            </Link>
+          </li>
+        </ul>
+      </div>
+    );
   };
 
   return (
@@ -30,14 +54,14 @@ function Header() {
             About
           </Link>
           {user ? (
-            <Link to="/profile" className="nav-link">
-              My Profile
-            </Link>
-          ) : null}
-          {user ? (
-            <button onClick={handleLogOut} className="nav-link logout-button">
-              Logout
-            </button>
+            <div
+              className="nav-link dropdown"
+              onMouseEnter={handleMouseEnter}
+              onMouseLeave={handleMouseLeave}
+            >
+              <button className="cta-button"> Account ▼ </button>
+              {isDropdownVisible && profileDropdown()}
+            </div>
           ) : (
             <Link to="/login" className="nav-link">
               <button className="cta-button">Share Your Idea</button>
