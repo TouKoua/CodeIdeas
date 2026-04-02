f89e394098e1e79e0279bde9d851b8269dc1a334
TouKoua <tou-koua1@hotmail.com>
2026-04-02 14:43:29 -0500

    REVERTING CHANGE
    doing some more testing. Reverting the change I did

--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -82,7 +82,7 @@ function ProfilePage() {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
-            Authorization: `Bearer ${import.meta.env.VITE_SUPABASE_ANON_KEY}`,
+            Authorization: `Bearer ${session?.access_token}`,
           },
           body: JSON.stringify({ userId: user?.id }),
         },
